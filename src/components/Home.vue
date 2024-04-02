8123731f3463b0d2daf29b32d79673cea9bb922c
iampeterjohn <peterjeche442@gmail.com>
2024-04-02 11:01:38 -1100

    books items

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,7 +10,7 @@
             <div v-if="showBooks">
                 <ul>
                     <!-- v-for is use for list rendering, where by the first property represent each item in the array while the second property represent the array object -->
-                    <li v-for="book in books" :class="{ fav : book.isFav }">
+                    <li v-for="book in books" :class="{ fav : book.isFav }" @click="toggleFav(book)">
                         <!-- v-bind is use to bind data property to html dynamically, this take the value from your script and replace it with your html element dynamically -->
                         <a :href="book.url"><img :src="book.img" :alt="book.title" loading="lazy"></a>
                         <p>{{ book.title }}</p>
@@ -64,46 +64,26 @@
     methods:{
         toggleBtn(){
             this.showBooks = !this.showBooks
+        },
+
+        toggleFav(book){
+            book.isFav = !book.isFav;
         }
-    }
+    },
+
+    // computed:{
+    //     filterBook(){
+    //        return this.books.filter( (book) => book.isFav)
+    //     }
+    // }
   }
 </script>
 
 <style scoped>
-  li{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: white;
-    padding: 10px;
-    margin: 1rem 0;
-    border: 1px solid transparent;
-    border-radius: 10px;
-    font-weight: 600;
-    text-transform: capitalize;
-    list-style: none;
-  }
-
-  img{
-    width: 80px;
-    object-fit: cover;
-  }
 
   .fav{
     background-color: pink;
     color: white;
-  }
-
-  .my-books{
-    text-align: center;
-    text-transform: capitalize;
-    color: white;
-    width: 50%;
-    margin: 2rem auto;
-    background-color: #27ec27;
-    font-weight: 600;
-    padding: 1rem 0;
-    box-shadow: 1px 1px 3px 3px  #27ec27;
   }
 
   button{
@@ -149,6 +129,35 @@
    3. boolean 
    4. array
    5. object
-   6 null etc. -->
+   6 null etc. 
+
+   V-ON DIRECTIVE
+  
+   2 TYPES OF EVENT HANDLERS IN VUE INCLUDES:
+
+   i. inline handler
+   ii. methods handler
+   
+   * inline handler make you write javascipt function directly in the attributes while  methods handler is just mentioning the method name which was declared in the script,here we make use of method handler, the method handler is use to target where the function is been declared in the script and once the element it's been assigned to is clicked, the function will execute immediately if there's no bug holding back.
+
+   V-IF, V-ELSE AND V-SHOW DIRECTIVES
+
+   these are use for list rendering base on conditions
+    
+   ==> v-if directive = is use only if the condition is true but if the condition is false the v-else directive is render, the v-else must be declare after v-if directive if not it will not work.
+  ==> v-show directive = this is a css property that is use to change the css property of an element i.e it change the display property of an element to none if it been set to FALSE and set it to block if it been set to TRUE, the v-show directive do not destroy the element from Dom rather set the css property to none but the element will be present in the Dom.
+
+  DIFFERENCE BETWEEN V-IF AND V-SHOW 
+
+  1. v-if destroy the property completely from the DOM while v-show hide the property in  the DOM but not remove it using css property 'display: none'.
+  2. v-if is use when you want to remove an element completely from DOM while v-show is use when you want to toggle through and elements e.t.c
+
+  DIFFERNCE BETWEEN V-IF AND V-FOR 
+
+  1. v-if must be assign to an element before v-for to avoid breakage  of dom structure.
+  2. v-for must not be at the top of v-if in thesame container
+  3. v-if as higher value than v-for
+-->
+
 
 
